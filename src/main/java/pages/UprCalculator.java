6bb55f6c1437d273905cfa02766d57cd2fd099a2
HYPERPC <alexandr.markov1997@yandex.ru>
2022-02-11 19:05:44 +0400

    Заготовка проекта

--- src/main/java/pages/UprCalculator.java
+++ src/main/java/pages/UprCalculator.java
@@ -1,4 +1,59 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
 public class UprCalculator {
+    @FindBy (css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"N10B9\"]")
+    public WebElement one;
+
+    @FindBy (css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"lVjWed\"]")
+    public WebElement two;
+
+    @FindBy (css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"KN1kY\"]")
+    public WebElement three;
+
+    @FindBy (css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"xAP7E\"]")
+    public WebElement four;
+
+    @FindBy (css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"Ax5wH\"]")
+    public WebElement five;
+
+    @FindBy (css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"abcgof\"]")
+    public WebElement six;
+
+    @FindBy (css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"bkEvMb\"]")
+    public WebElement zero;
+
+    @FindBy (css = "div[aria-label=\"закрывающая скобка\"]")
+    public WebElement rightsc;
+
+    @FindBy (css = "div[aria-label=\"открывающая скобка\"]")
+    public WebElement leftsc;
+
+    @FindBy (css = "div[aria-label=\"вычитание\"]")
+    public WebElement minus;
+
+    @FindBy (css = "div[aria-label=\"сложение\"]")
+    public WebElement plus;
+
+    @FindBy (css = "div[aria-label=\"умножение\"]")
+    public WebElement increase;
+
+    @FindBy (css = "div[aria-label=\"деление\"]")
+    public WebElement part;
+
+    @FindBy (css = "div[aria-label=\"равно\"]")
+    public WebElement equals;
+
+    @FindBy (css = "div[aria-label=\"синус\"]")
+    public WebElement sin;
+
+
+    public UprCalculator(WebDriver driver) { initElements(driver, this);}
+
+
 }
--- src/test/java/tests/googleTest.java
+++ src/test/java/tests/googleTest.java
@@ -4,15 +4,21 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import pages.SearchPage;
+import pages.UprCalculator;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class googleTest {
     private static WebDriver driver;
     private static SearchPage searchPage;
+    private static UprCalculator UprCalculator;
 
     @BeforeAll
     public static void init() {
@@ -21,17 +27,69 @@ public class googleTest {
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
         searchPage = new SearchPage(driver);
+        UprCalculator = new UprCalculator(driver);
+
     }
     @BeforeEach
     public void setup(){ driver.get("https://google.com/"); }
 
     @Test
     public void calculator1() {
-       searchPage.search("Calculator");
+       searchPage.search("Калькулятор");
+       UprCalculator.leftsc.click();
+        //(
+       UprCalculator.one.click();
+       //1
+       UprCalculator.plus.click();
+       //+
+       UprCalculator.two.click();
+       //2
+       UprCalculator.rightsc.click();
+       //)
+       UprCalculator.increase.click();
+       //*
+       UprCalculator.three.click();
+       //3
+       UprCalculator.minus.click();
+       //-
+       UprCalculator.four.click();
+       //4
+       UprCalculator.zero.click();
+       //0
+       UprCalculator.part.click();
+       //:
+       UprCalculator.five.click();
+       //5
+       UprCalculator.equals.click();
+       //=
+       assertAll(() -> assertEquals("(1 + 2) × 3 - 40 ÷ 5 =", driver.findElement(By.cssSelector("div [jsname=\"VkJw6\"] span")).getText()),
+               () -> assertEquals("1", driver.findElement(By.cssSelector("div [jsname=\"zLiRgc\"] span")).getText()));
 
     }
-    @AfterAll
-    public static void teardown() {
-        driver.quit();
+    @Test
+    public void calculator2(){
+        searchPage.search("Калькулятор");
+        UprCalculator.six.click();
+        //6
+        UprCalculator.part.click();
+        //:
+        UprCalculator.zero.click();
+        //0
+        UprCalculator.equals.click();
+        //=
+        assertAll(() -> assertEquals("(1 + 2) × 3 - 40 ÷ 5 =", driver.findElement(By.cssSelector("div [jsname=\"VkJw6\"] span")).getText()),
+                () -> assertEquals("1", driver.findElement(By.cssSelector("div [jsname=\"zLiRgc\"] span")).getText()));
     }
+    @Test
+    public void calculator3(){
+        searchPage.search("Калькулятор");
+        UprCalculator.sin.click();
+        //sin
+        UprCalculator.equals.click();
+        //=
+        assertAll(() -> assertEquals("(1 + 2) × 3 - 40 ÷ 5 =", driver.findElement(By.cssSelector("div [jsname=\"VkJw6\"] span")).getText()),
+                () -> assertEquals("1", driver.findElement(By.cssSelector("div [jsname=\"zLiRgc\"] span")).getText()));
+    }
+    @AfterAll
+    public static void teardown() {driver.quit(); }
 }
