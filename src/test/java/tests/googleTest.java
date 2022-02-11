23cdeeeeadeec6b1e4277556c34441db29fd0621
HYPERPC <alexandr.markov1997@yandex.ru>
2022-02-11 17:37:52 +0400

    Заготовка проекта

--- src/test/java/tests/googleTest.java
+++ src/test/java/tests/googleTest.java
@@ -1,30 +1,37 @@
 package tests;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pages.SearchPage;
 
 
 public class googleTest {
     private static WebDriver driver;
+    private static SearchPage searchPage;
 
     @BeforeAll
-    public static void init(){
+    public static void init() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
+        searchPage = new SearchPage(driver);
     }
-    @Test
-    public void calculator()
-    {
-        driver.get("https://www.google.com/");
-        driver.findElement(By.xpath("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
-        driver.quit();
+    @BeforeEach
+    public void setup(){ driver.get("https://google.com/"); }
 
+    @Test
+    public void calculator1() {
+       searchPage.search("Calculator");
+
+    }
+    @AfterAll
+    public static void teardown() {
+        driver.quit();
     }
 }
